88ff76809874549a3b2f7d81bb755a0015f88aba
Abraham Flores <abrahamjhared.flores@gmail.com>
2024-02-10 14:48:42 -0800

    REDACT: first explorations of the dataset

--- scripts/exploration.py
+++ scripts/exploration.py
@@ -1,28 +1,43 @@
 # * Import proper Data Analysis pipeline tools
 import pandas as pd
 
-def getAttendanceNumbersPerWeek(df_csv):
-    
+def getAttendanceNumbersPerDayOfTheWeek(df_csv):
+
     # to datetime format...
     df_csv['fecha_mmddyyyy'] = pd.to_datetime(df_csv['fecha_mmddyyyy'], 
                                               format='%A, %B %d, %Y')
     
-    # new column to display the day of the week
-    # TODO review starting day and end day of each workweek
-    df_csv['day_of_week'] = df_csv['fecha_mmddyyyy'].dt.day_name()
-    df_csv['start_workweek'] = df_csv.groupby([
-        df_csv['fecha_mmddyyyy'].dt.isocalendar().week])['fecha_mmddyyyy'].transform('min')
-    df_csv['end_workweek'] = df_csv.groupby([
-        df_csv['fecha_mmddyyyy'].dt.isocalendar().week])['fecha_mmddyyyy'].transform('max')
+    # Define the desired order of days of the week
+    desired_order = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
 
-    # grouping by week, counting the attendance registers
-    attendance_by_week = df_csv.groupby([df_csv['fecha_mmddyyyy'].
-                                         dt.isocalendar().week, 
-                                         'start_workweek', 'end_workweek']).size().reset_index(
-                                             name='attendance_count')
+    # Create a mapping of the day names to their corresponding position in the desired order
+    order_mapping = {day: idx for idx, day in enumerate(desired_order)}
+
+    # Group by day of the week
+    attendance_by_day = df_csv.groupby(df_csv['fecha_mmddyyyy'].dt.day_name()).size().reset_index(name='attendance_count')
+
+    # Apply the custom sorting key based on the desired order
+    attendance_by_day_sorted = df_csv.sort_values(by='fecha_mmddyyyy', key=lambda x: x.map(order_mapping))
+
+    # Display the result
+    print("ATTENDANCE BY DAY:")
+    print(attendance_by_day_sorted)
+
+def getAttendanceNumbersPerMatricula(df_csv):
+    # Group the DataFrame by the "matricula" column and calculate the sum of attendance counts for each group
+    attendance_by_matricula = df_csv.groupby('matricula').size().reset_index(name="attendance_count")
+
+    # Display the result
+    print("ATTENDANCE BY MATRICULA:")
+    print(attendance_by_matricula)
+
+    # Sort the DataFrame based on the "attendance_count" column in descending order
+    top_ten_attendance_records = attendance_by_matricula.sort_values(by='attendance_count', ascending=False).head(10)
     
-    print("ATTENDANCE BY WEEK: ")
-    print(attendance_by_week)
+    # Display the result
+    print("TOP 10 ATTENDANCE BY MATRICULA:")
+    print(top_ten_attendance_records)
+
 
 def main():
     ATTENDANCE_DATA_CSV_FILENAME = "../csv/attendance_data.csv"
@@ -31,7 +46,8 @@ def main():
 
     #print(df_csv)
 
-    getAttendanceNumbersPerWeek(df_csv)
+    getAttendanceNumbersPerDayOfTheWeek(df_csv)
+    getAttendanceNumbersPerMatricula(df_csv)
 
 if __name__ == '__main__':
     main()
